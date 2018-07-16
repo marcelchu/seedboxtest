3d6af1c4c30156796b8d9251951648003244b836
marcelchu <marcel_chu@hotmail.com>
2018-07-16 02:30:18 -0400

    Upgrade visibility

--- SeedBox_test.py
+++ SeedBox_test.py
@@ -6,7 +6,6 @@ Created on Fri Jul 13 12:07:06 2018
 Title: SeedBox Technologies Test
 """
 #Importing the libraries
-import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -27,6 +26,8 @@ control_group_list = testsamples.loc[testsamples['test_group'] == 0]
 p_test_group = len(test_group_list)/len(testsamples)
 p_control_group = 1-p_test_group
 
+print("\n1.What is the aproximate probability distribution between the test group and the control group?\n")
+
 print("The aproximate probability distribution for the test group (must call-in) is " 
       + str("{0:.2f}".format(p_test_group)) + " and " 
       + str("{0:.2f}".format(p_control_group)) 
@@ -35,7 +36,7 @@ print("The aproximate probability distribution for the test group (must call-in)
       + " people placed in the must call-in to cancel test group and "
       + str(len(control_group_list)) 
       + " people placed in the web form test group. The total population studied is "
-      + str(len(testsamples)) + ".")
+      + str(len(testsamples)) + ".\n\n")
 
 #Data to plot.
 labels = 'Must call-in', 'Web-form'
@@ -84,6 +85,9 @@ average_rebill_test = total_rebill_count_test/len(test_group_list)
 #Number of REBILL difference.
 difference = (total_rebill_count_test/len(test_group_list))-(total_rebill_count_control/len(control_group_list))
 
+
+print("2 Is a user that must call-in to cancel more likely to generate at least 1 addition REBILL?\n")
+
 print("With the given data, we know that "
       + str(len(control_group_list)) + " web-form users do in total "
       + str(total_rebill_count_control) + " REBILLS. Furthermore, "
@@ -92,11 +96,11 @@ print("With the given data, we know that "
       + str("{0:.2f}".format(average_rebill_control))
       + " REBILLs per user and the average amount of REBILLs for must call-in users is " 
       + str("{0:.2f}".format(average_rebill_test)) 
-      + " REBILLs per user. If we do a substraction between the amounts, we can see that\
-      the must call-in to cancel generate in average "
+      + " REBILLs per user. If we do a substraction between the amounts, we can see that"
+      + "the must call-in to cancel generate in average "
       + str("{0:.2f}".format(difference)) 
-      + " more REBILLS than the web-form.\
-      It is not 1 additionnal REBILL, but this value shows that the must call-in group helps the company make more profit, because they don't want to call to make an cancellation.")
+      + " more REBILLS than the web-form."
+      + "It is not 1 additionnal REBILL, but this value shows that the must call-in group helps the company make more profit, because they don't want to call to make an cancellation.\n\n")
 
 """
 3 Is a user that must call-in to cancel more likely to generate more revenues?
@@ -118,6 +122,8 @@ AVGtransAmount_testGroup = totaltransAmount_testGroup/len(test_group_list)
 #Revenu difference
 revenue_difference = AVGtransAmount_testGroup - AVGtransAmount_controlGroup
 
+print("3 Is a user that must call-in to cancel more likely to generate more revenues?\n")
+
 print("In total, the web-form users generate "
       + str("{0:.2f}".format(totaltransAmount_controlGroup)) + "$ and the must call-in users generate "
       + str("{0:.2f}".format(totaltransAmount_testGroup)) + "$." 
@@ -125,12 +131,12 @@ print("In total, the web-form users generate "
       + str("{0:.2f}".format(AVGtransAmount_controlGroup))
       + "$ per web-form user and the average revenue amount is "
       + str("{0:.2f}".format(AVGtransAmount_testGroup))
-      + "$ per must call-in user. With this information,\
-      we can confirm that the must call-in to cancel user is more likely\
-      to generate more revenues than the web-form to cancel user.\
-      In fact, with a simple substraction, the must-call in to cancel test group generate in average "
-      + str("{0:.2f}".format(revenue_difference) + "$ more per user than the web form test group.\
-            People don't want to call-in to cancel their subscription therefore they still have to pay."))
+      + "$ per must call-in user. With this information,"
+      + "we can confirm that the must call-in to cancel user is more likely"
+      + "to generate more revenues than the web-form to cancel user."
+      + "In fact, with a simple substraction, the must-call in to cancel test group generate in average "
+      + str("{0:.2f}".format(revenue_difference)) + "$ more per user than the web form test group."
+      + "People don't want to call-in to cancel their subscription therefore they still have to pay.\n\n")
 
 CallIn_transactions = transaction_merged.loc[transaction_merged['test_group'] == 1]
 
@@ -187,6 +193,8 @@ totalChargeBack_WebForm = len(WebForm_list[WebForm_list["transaction_type"]=='CH
 ChargebackRate_CallIn = (totalChargeBack_CallIn/totalRebill_CallIn)*100
 ChargebackRate_WebForm = (totalChargeBack_WebForm/totalRebill_WebForm)*100
 Chargeback_difference = ChargebackRate_WebForm - ChargebackRate_CallIn
+
+print("4 Is a user that must call-in more likely to produce a higher chargeback rate(CHARGEBACKs/REBILLs)\n")
 
 print("With the informations, we know that the must call-in users get in total "
       + str(totalRebill_CallIn) + " rebills and " + str(totalChargeBack_CallIn)
